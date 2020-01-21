cfb0f87497e3d43e1e886659273b5013c340529a
markhorvath <markhorvath7@gmail.com>
2020-01-20 20:10:12 -0500

    added createDate, spit out string for date

--- src/index.js
+++ src/index.js
@@ -8,28 +8,38 @@ import ApprovalCard from './ApprovalCard';
 
 //create react component
 const App = () => {
-
+    function createDate() {
+        //use faker to generate a random Datetime object in the past
+        let date = faker.date.past();
+        var d = date.getDate();
+        var m = date.getMonth() + 1;
+        //convert datetime object to string
+        var dateStr = m + "/" + d + "/" + date.getFullYear();
+        console.log(dateStr);
+        return dateStr;
+    }
+    createDate();
     return (
     <div className="ui container comments">
         <ApprovalCard>
             <CommentDetail
                 imgSrc={faker.image.avatar()}
                 author={faker.name.firstName()}
-                date={faker.date.month()}
+                date={createDate()}
                 text={faker.lorem.sentence()}/>
         </ApprovalCard>
         <ApprovalCard>
             <CommentDetail
                 imgSrc={faker.image.avatar()}
                 author={faker.name.firstName()}
-                date={faker.date.month()}
+                date={createDate()}
                 text={faker.lorem.sentence()}/>
         </ApprovalCard>
         <ApprovalCard>
             <CommentDetail
                 imgSrc={faker.image.avatar()}
                 author={faker.name.firstName()}
-                date={faker.date.month()}
+                date={createDate()}
                 text={faker.lorem.sentence()}/>
         </ApprovalCard>
     </div>
